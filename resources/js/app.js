a28b9f8d6a6c420544a3c563625aea7cfc362770
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-06-24 18:22:21 -0400

    item, form & list components and initial styling

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,5 +4,12 @@ import { createApp } from 'vue';
 
 import App from './vue/app';
 
-const app = createApp(App);
-app.mount('#app');
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPlusSquare, faTrash);
+
+const app = createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app');
--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,11 +1,37 @@
 <template>
-    <div>
-        Vue App
+    <div class="app-container">
+        <div class="top-section">
+            <h2 id="title">List App</h2>
+            <itemForm/>
+        </div>
+        <itemsList/>
     </div>
 </template>
 
 <script>
-export default {
+import itemForm from './itemForm';
+import itemsList from './itemsList';
 
+export default {
+    components: {
+        itemForm,
+        itemsList
+    }
 }
 </script>
+
+<style scoped>
+    .app-container {
+        width: 350px;
+        margin: auto;
+    }
+
+    .top-section {
+        background: #e6e6e6;
+        padding: 10px;
+    }
+
+    #title {
+        text-align: center;
+    }
+</style>
